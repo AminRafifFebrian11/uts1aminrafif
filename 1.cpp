f18718fb7ee9fd56fea9e887fa33f56ea97a1f44
AminRafifFebrian11 <49119429+AminRafifFebrian11@users.noreply.github.com>
2019-03-31 21:17:13 +0700

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "nama : Amin Rafif Febrian" <<endl;
+    cout <<"Nim : 1829142034"<<endl;
+    cout <<"jurusan : TEKOM B"<<endl;
+    cout <<"Tahun : 2018"<<endl;
+}
